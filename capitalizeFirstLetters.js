2f2cd77a902da2a434b284bfe580230648513420
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2020-12-15 00:11:17 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,4 +1,5 @@
 // capitalizeFirstLetters.js
+const assert = require("assert");
 
 function capitalizeFirstLetters(str) {
 	return str
@@ -7,4 +8,18 @@ function capitalizeFirstLetters(str) {
 		.join(" ");
 }
 
-module.exports = capitalizeFirstLetters;
+// Est bien une fonction qui accepte un argument
+assert.strictEqual(typeof capitalizeFirstLetters, "function");
+assert.strictEqual(capitalizeFirstLetters.length, 1);
+
+// Transforme correctement en javaScript
+assert.strictEqual(capitalizeFirstLetters("javaScript"), "JavaScript");
+
+// Fonctionne avec 1 caractère
+assert.strictEqual(capitalizeFirstLetters("z"), "Z");
+
+// "Fonctionne avec plusieurs mots
+assert.strictEqual(capitalizeFirstLetters("plusieurs mots"), "Plusieurs Mots");
+
+// Fonctionne avec un chaîne vide
+assert.strictEqual(capitalizeFirstLetters(""), "");
